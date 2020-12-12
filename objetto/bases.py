518f48dd618b55d9beb33841d6d954e4b528f0bb
Bruno Nicko <brunonicko@gmail.com>
2020-12-11 17:35:18 -0800

    Upversion

--- objetto/bases.py
+++ objetto/bases.py
@@ -28,8 +28,8 @@ from ._bases import (
     final,
     init,
     init_context,
-    simplify_member_names,
     make_base_cls,
+    simplify_member_names,
 )
 from ._changes import BaseAtomicChange, BaseChange
 from ._data import (
